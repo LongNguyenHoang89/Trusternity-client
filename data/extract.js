ec282677e4f1629077dd8f0be20cd3f986a577f0
Long Nguyen <LongNguyenHoang@Outlook.com>
2017-07-10 17:47:14 +0200

    basic data extracting function - very slow

--- data/extract.js
+++ data/extract.js
@@ -3,13 +3,11 @@
 */
 
 var Web3 = require('web3');
+var fs = require('fs');
 
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
 
-var num = 100;
-web3.eth.getBlock(num, callback);
-
-function callback(error, block){
+function web3Callback(error, block){
     if (error != undefined){
         console.log("something happens")
     }
@@ -18,3 +16,43 @@ function callback(error, block){
         console.log(result);
     }
 }
+
+function extractData(fileName){    
+    var header = 'num, diff, time, interval, avg\n';
+    fs.writeFileSync(fileName + '.csv', header);
+    var prev = 0;
+    var prev10 = [];
+
+    var currentBlockNum = 2000000;
+    //var currentBlockNum = 10;
+
+    for (var i = 0; i < currentBlockNum; i++) {        
+        var block = web3.eth.getBlock(i);
+        // for each block, we calculate a different between 2 timestamps
+        var timeDifferent = block.timestamp - prev;
+        prev = block.timestamp;
+
+        // now we skip the first 3 blocks, they are stablizing anyway
+        if (i < 3) continue;
+
+        // for each block, we calculate an average time different of 10 blocks
+        // from the selected one to 9 previous others
+        if (prev10.length == 10) {
+            prev10.shift(); //remove the first element from queue
+        }
+        prev10.push(timeDifferent); //push this one
+        var avg = prev10.reduce(function (sum, value) {
+            return sum + value;
+        }, 0) / prev10.length;
+
+        var line = block.number
+            + "," + block.difficulty
+            + "," + block.timestamp
+            + "," + timeDifferent
+            + "," + avg
+            + "\n";
+        fs.appendFileSync(fileName + '.csv', line);
+    }
+}
+
+extractData('test');
