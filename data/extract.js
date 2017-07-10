9e0baa9333fd6fbcf97f40c36a30e0e727b3aae6
longnguyenhoang89 <longnguyenhoang@outlook.com>
2017-07-10 18:36:41 +0200

    update scripts with 10 parralel tasks

--- data/extract.js
+++ data/extract.js
@@ -4,55 +4,36 @@
 
 var Web3 = require('web3');
 var fs = require('fs');
+var async = require('async');
+
+//var resultFileName = 'test';
 
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
 
-function web3Callback(error, block){
-    if (error != undefined){
-        console.log("something happens")
-    }
-    else {
-        var result = block.number
-        console.log(result);
-    }
+function extractData(id, callback) {
+    var block = web3.eth.getBlock(id);    
+    callback(block);
 }
 
-function extractData(fileName){    
-    var header = 'num, diff, time, interval, avg\n';
-    fs.writeFileSync(fileName + '.csv', header);
-    var prev = 0;
-    var prev10 = [];
+function runQueue(resultFileName) {
+    var queue = async.queue(extractData, 10); // Run 10 concurrent extract    
+    queue.drain = function () {
+        console.log('job finished');
+    }
+
+    var header = 'num, diff, time\n';
+    fs.writeFileSync(resultFileName + '.csv', header);
 
     var currentBlockNum = 2000000;
-    //var currentBlockNum = 10;
-
-    for (var i = 0; i < currentBlockNum; i++) {        
-        var block = web3.eth.getBlock(i);
-        // for each block, we calculate a different between 2 timestamps
-        var timeDifferent = block.timestamp - prev;
-        prev = block.timestamp;
-
-        // now we skip the first 3 blocks, they are stablizing anyway
-        if (i < 3) continue;
-
-        // for each block, we calculate an average time different of 10 blocks
-        // from the selected one to 9 previous others
-        if (prev10.length == 10) {
-            prev10.shift(); //remove the first element from queue
-        }
-        prev10.push(timeDifferent); //push this one
-        var avg = prev10.reduce(function (sum, value) {
-            return sum + value;
-        }, 0) / prev10.length;
-
-        var line = block.number
-            + "," + block.difficulty
-            + "," + block.timestamp
-            + "," + timeDifferent
-            + "," + avg
-            + "\n";
-        fs.appendFileSync(fileName + '.csv', line);
+    for (var i = 0; i < currentBlockNum; i++) {
+        queue.push(i, function (block) {            
+            var line = block.number
+                + "," + block.difficulty
+                + "," + block.timestamp
+                + "\n";
+            fs.appendFileSync(resultFileName + '.csv', line);
+        });
     }
 }
 
-extractData('test');
+runQueue('test');
