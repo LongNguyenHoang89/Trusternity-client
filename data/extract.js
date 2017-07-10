840845674e93fbf723af0f6b63fd46398ac83f51
Long Nguyen <LongNguyenHoang@Outlook.com>
2017-07-10 16:27:59 +0200

    update extract.js

--- data/extract.js
+++ data/extract.js
@@ -1,3 +1,20 @@
 /*
     We extract real data from Ethereum canonical blockchain header.     
 */
+
+var Web3 = require('web3');
+
+web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+
+var num = 100;
+web3.eth.getBlock(num, callback);
+
+function callback(error, block){
+    if (error != undefined){
+        console.log("something happens")
+    }
+    else {
+        var result = block.number
+        console.log(result);
+    }
+}
