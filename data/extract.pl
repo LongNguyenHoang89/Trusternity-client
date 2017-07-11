e7c7468ad49f7b4afa7a00e1857182b9212086d2
longnguyenhoang89 <longnguyenhoang@outlook.com>
2017-07-11 15:26:49 +0200

    add some comments for extract.pl

--- data/extract.pl
+++ data/extract.pl
@@ -1,3 +1,6 @@
+# Somehow Geth does not response after every 11k request.
+# This perl script runs extract.js in a process for every 1000 request.
+
 $max = 4000000;
 $counter = 44004;
 $interval = 1000;
